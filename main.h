ef69a56f2df347db1855030669a2fde9c38b4440
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-27 02:03:57 +0000

    P0

--- main.h
+++ main.h
@@ -1,6 +1,28 @@
-#ifndef PRINTF_H 
-#define PRINTF_H
+#ifndef MAIN_H
+#define MAIN_H
 
+/* librarys */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/* function prototypes */
 int _printf(const char *format, ...);
+int get_function(char c, va_list args);
+int _putchar(char c);
+
+/* type conversion specifiers */
+int put_char(va_list args);
+int put_str(va_list args);
+int put_digt(va_list args);
+int put_mod(va_list args);
+
+/* matcher list */
+typedef struct match_er
+{
+char t;
+int (*f)(va_list);
+} f_t;
 
 #endif

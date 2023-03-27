882305ddf66cd4a7acde5dc43f50191372f37164
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-27 03:00:59 +0000

    P1

--- _putchar.c
+++ _putchar.c
@@ -1,27 +1,13 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _putchar - a function that writes a given character to stdout
- * @c: given character
+ * _putchar - a function that writes a given character to stdout.
+ * @c: given character.
  *
  * Return: 1 if successful, else return -1 on error.
  */
 
 int _putchar(char c)
 {
-  static char buf[1024];
-	static int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buf, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buf[i] = c;
-		i++;
-	}
-	return (1);
+return (write(1, &c, 1));
 }
